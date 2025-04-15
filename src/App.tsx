d273f383724b59bbd967985b08f0706f78893a19
nguyenthanhhung <nguyenthanhhung.dev@gmail.com>
2025-04-15 12:54:26 +0700

    Thêm route cho product detail

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './app/home/page';
 import CartPage from './app/cart/page';
+import ProductDetailPage from '@/app/productDetail/page';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import ProductGridPage from './app/productGrid/page';
@@ -14,7 +15,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/men/sneakers" element={<ProductGridPage />} />
-        </Routes>
+          <Route path="/product/:id" element={<ProductDetailPage />} />
+      </Routes>
         <Footer />
       </div>
     </Router>
